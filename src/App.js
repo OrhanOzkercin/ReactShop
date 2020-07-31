4d2873a8fb35003bc8a6a7ca007d112d61266f3c
orhanozkercin <orhanozkercin@yahoo.com>
2020-07-31 16:20:44 +0300

    First commit, created React project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import HomePage from './pages/homepage/Homepage';
+
+function App() {
+	return (
+		<div className='App'>
+			<HomePage />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './Homepage.scss';
+
+const HomePage = () => {
+	return (
+		<div className='homepage'>
+			<div className='directory'>
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>HATS</h1>
+						<span className='subtitle'>SHOP NOW</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default HomePage;

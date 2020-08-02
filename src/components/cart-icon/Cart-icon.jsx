fac47627b5fadcd82939f3828c715b438ec7ed10
orhanozkercin <orhanozkercin@yahoo.com>
2020-08-02 11:43:16 +0300

    Some improvements to Cart-icon components

--- src/components/cart-icon/Cart-icon.jsx
+++ src/components/cart-icon/Cart-icon.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 
 import { ReactComponent as Basket } from '../../assests/basket.svg';
 import './Cart-icon.scss';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 const CartIcon = () => {
 	const dispatch = useDispatch();
-
+	const itemsCount = useSelector((state) => {
+		console.log('called');
+		return state.cart.cartItems.reduce(
+			(acc, cartItem) => acc + cartItem.quantity,
+			0
+		);
+	});
 	const ToggleCartHidden = () => dispatch(toggleCartHidden());
 	return (
 		<div className='cart-icon' onClick={() => ToggleCartHidden()}>
 			<Basket className='shopping-icon' />
-			<span className='item-count'>0</span>
+			<span className='item-count'>{itemsCount}</span>
 		</div>
 	);
 };
